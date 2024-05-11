2d2e2c79e2b7852998b2b655bf9a4a1fda887b27
silvanioaguiar <95724451+silvanioaguiar@users.noreply.github.com>
2024-05-11 10:19:45 -0300

    Alterado class appModule  Response

--- backend/lib/src/app_module.dart
+++ backend/lib/src/app_module.dart
@@ -6,7 +6,7 @@ import 'package:shelf_modular/shelf_modular.dart';
 class AppModule extends Module {
   @override
   List<ModularRoute> get routes => [
-        Route.get('/', (Request request) => Response.ok('Inicial')),
+        Route.get('/', (Request request) => Response.ok('Teste_Inicial')),
         Route.resource(UserResource())
       ];
 }
