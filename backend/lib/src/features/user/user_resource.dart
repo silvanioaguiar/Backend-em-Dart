69f8ba2a0248fa5266173b403bc2f09f8915fc77
silvanioaguiar <95724451+silvanioaguiar@users.noreply.github.com>
2024-05-11 09:41:23 -0300

    teste primeira alteração

---
+++ backend/lib/src/features/user/user_resource.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf_modular/shelf_modular.dart';
+
+class UserResource extends Resource {
+  @override
+  List<Route> get routes => [
+        Route.get('/user', _getAllUser),
+        Route.get('/user/:id', _getUserByid),
+        Route.post('/user', _createUser),
+        Route.put('/user', _updateUser),
+        Route.delete('/user/:id', _deleteUser),
+      ];
+
+  FutureOr<Response> _getAllUser() {
+    return Response.ok('ok');
+  }
+
+  FutureOr<Response> _getUserByid(ModularArguments arguments) {
+    return Response.ok('User: ${arguments.params['id']}');
+  }
+
+  FutureOr<Response> _createUser(ModularArguments arguments) {
+    return Response.ok('Created user: ${arguments.data}');
+  }
+
+    FutureOr<Response> _updateUser(ModularArguments arguments) {
+    return Response.ok('Updated user: ${arguments.data}');
+  }
+
+      FutureOr<Response> _deleteUser(ModularArguments arguments) {
+    return Response.ok('Deleted user: ${arguments.params['id']}');
+  }
+}
